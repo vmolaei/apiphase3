2a996a09fc3a27ed8117cfcc40bc6b5c51546f43
Vahid Molaei Bejarbaneh <91679087+vmolaei@users.noreply.github.com>
2021-10-19 12:03:14 +0330

    second commit:Laravel api with JWT authentication

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Course;
+
+class CourseController extends Controller
+{
+    //Course Enrollment Api --POST
+    public function courseEnrollment(Request $request){
+
+    }
+    //total course Api -GET
+    public function totalCourses(){
+
+    }
+    //delete Course Api --GET
+    public function deleteCourse($id){
+
+    }
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(){
+
+    }
+    public function login(){
+
+    }
+    public function profile(){
+
+    }
+    public function logout(){
+
+    }
+}
