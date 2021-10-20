0e7c50b96a9d9f7f6d0b69fca4c9eae9a8d624b5
Vahid Molaei Bejarbaneh <91679087+vmolaei@users.noreply.github.com>
2021-10-20 11:13:21 +0330

    third commit:Laravel api with JWT authentication

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,10 +5,34 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function register(){
+    public function register(Request $request){
+        //validation
+        $request->validate([
+            "name"=>"required",
+            "email"=>"required|email|unique:users",
+            "phone_no"=>"required",
+            "password"=>"required|confirmed"
+
+        ]);
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone_no = $request->phone_no;
+        $user->password = bcrypt($request->password);
+
+        //save data
+
+        $user->save();
+
+        //send response
+        return response()->json([
+            "status"=>1,
+            "message"=>"you registered successfully"
+        ]);
 
     }
     public function login(){
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post("register",[UserController::class,"register"]);
+Route::post("login",[UserController::class,"login"]);
+
+Route::group(["middleware"=>["api"]],function(){
+    Route::get("profile",[UserController::class,"profile"]);
+    Route::get("logout",[UserController::class,"logout"]);
+
+    //course Api Routes
+    Route::post("course-enrollment",[CourseController::class,"courseEnrollment"]);
+    Route::get("total-courses",[CourseController::class,"totalCourses"]);
+    Route::get("delete-course/{id}",[CourseController::class,"deleteCourse"]);
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
